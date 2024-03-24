2bf8a196d71b7b32c44bf34f66163ebfd38d4265
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-24 03:42:30 +0300

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Jafar on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
