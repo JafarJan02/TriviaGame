984b316ddf4f269b7314a493a85f70b9ab69303c
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-24 09:21:33 +0300

    Extension ''LilacTitle''

--- TriviaGame/Extansions.swift
+++ TriviaGame/Extansions.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Text {
+    func lilacTitle () -> some View {
+        self.font(.title)
+            .fontWeight(.heavy)
+            .foregroundColor(Color("AccentColor"))
+
+    }
+}
