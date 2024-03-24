d80c13cedfe09c016666f8108b45b010824df8e3
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-24 09:30:35 +0300

    QuestionView 1 out of 10

---
+++ TriviaGame/Views/QuestionView.swift
@@ -0,0 +1,34 @@
+//
+//  QuestionView.swift
+//  TriviaGame
+//
+//  Created by Jafar on 24.03.2024.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            HStack {
+                Text("Trivia Game")
+                    .lilacTitle()
+                Spacer()
+                Text("1 out of 10")
+                    .foregroundColor(Color("AccentColor"))
+                    .fontWeight(.heavy)
+                
+                
+            }
+        }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 0.984313725490196, green: 0.9294117647058824, blue: 0.8470588235294118))
+
+        
+    }
+}
+
+#Preview {
+    QuestionView()
+}
