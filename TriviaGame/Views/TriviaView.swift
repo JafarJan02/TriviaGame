2e0dc8fedf1688cb86f6ae379ad3427c73e1f773
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-25 22:48:56 +0300

    Creating TriviaView. Creating NavigationLink to QuestionView

---
+++ TriviaGame/Views/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  TriviaGame
+//
+//  Created by Jafar on 25.03.2024.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+#Preview {
+    TriviaView()
+}
