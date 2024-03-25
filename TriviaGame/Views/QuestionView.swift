535bfafd67a22c8417f910a1796d376d7bbde0cf
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-26 00:55:15 +0300

    Update the QuestionView

--- TriviaGame/Views/QuestionView.swift
+++ TriviaGame/Views/QuestionView.swift
@@ -15,27 +15,34 @@ struct QuestionView: View {
             HStack {
                 Text("Trivia Game")
                     .lilacTitle()
+                
                 Spacer()
-                Text("1 out of 10")
+                
+                Text("\(triviaManager.index + 1) out of \(triviaManager.length)")
                     .foregroundColor(Color("AccentColor"))
                     .fontWeight(.heavy)
                 
             }
             
-            ProgressBar(progress: 40)
+            ProgressBar(progress: triviaManager.progress)
             VStack(alignment: .leading, spacing: 20) {
-                Text("The capital of the US State Ohio is the city of Chillicothe.")
+                Text(triviaManager.question)
                     .font(.system(size: 20))
                     .bold()
                     .foregroundColor(.gray)
                 
-                AnswerRow(answer: Answer(text: "false", isCorrect: true))
-                    .environmentObject(triviaManager)
-                AnswerRow(answer: Answer(text: "true", isCorrect: false))
-                    .environmentObject(triviaManager)
+                ForEach(triviaManager.answerChoices, id: \.id) { answer in
+                    AnswerRow(answer: answer)
+                        .environmentObject(triviaManager)
+                }
             }
             
-            PrimaryButton(text: "Next")
+            Button {
+                triviaManager.goToNextQuestion()
+            } label: {
+                PrimaryButton(text: "Next", background: triviaManager.answerSelected ? Color("AccentColor") : Color(hue: 1.0, saturation: 0.0, brightness: 0.564, opacity: 0.327))
+            }
+            
             Spacer()
         }
             .padding()
