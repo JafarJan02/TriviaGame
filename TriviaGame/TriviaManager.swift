03a80d828932cdf761678c853a8cb9f57ed1ccb7
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-25 23:31:54 +0300

    Create the TriviaManager

---
+++ TriviaGame/TriviaManager.swift
@@ -0,0 +1,43 @@
+//
+//  TriviaManager.swift
+//  TriviaGame
+//
+//  Created by Jafar on 25.03.2024.
+//
+
+import Foundation
+
+class TriviaManager: ObservableObject {
+    private(set) var trivia: [Trivia.Result] = []
+    @Published private(set) var length = 0
+    
+    init() {
+        Task.init {
+            await fetchTrivia()
+                
+        }
+    }
+    
+    func fetchTrivia() async{
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=boolean") else {fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        do{
+           let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data")}
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(Trivia.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.trivia = decodedData.results
+                self.length = self.trivia.count
+            }
+        }catch{
+            print("Error fetching trivia: \(error) ")
+        }
+    }
+}
