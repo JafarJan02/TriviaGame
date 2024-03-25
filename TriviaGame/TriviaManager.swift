9d4642ee6b2d26de2642cbf4a350cd59a556ca21
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-25 23:56:02 +0300

    Set the next question

--- TriviaGame/TriviaManager.swift
+++ TriviaGame/TriviaManager.swift
@@ -6,12 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
 
 class TriviaManager: ObservableObject {
     private(set) var trivia: [Trivia.Result] = []
     @Published private(set) var length = 0
     @Published private(set) var index = 0
     @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.00
     
     init() {
         Task.init {
@@ -37,6 +42,7 @@ class TriviaManager: ObservableObject {
             DispatchQueue.main.async {
                 self.trivia = decodedData.results
                 self.length = self.trivia.count
+                self.setQuestion()
             }
         }catch{
             print("Error fetching trivia: \(error) ")
@@ -46,9 +52,19 @@ class TriviaManager: ObservableObject {
     func goToNextQuestion () {
         if index + 1 < length {
             index += 1
-            //Setting new question here...
+           setQuestion()
         }else{
             reachedEnd = true
+        }
+    }
+    func setQuestion() {
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length * 350))
+        
+        if index < length {
+            let currentTriviaQuestion = trivia[index]
+            question = currentTriviaQuestion.formattedQuestion
+            answerChoices = currentTriviaQuestion.answers
         }
     }
 }
